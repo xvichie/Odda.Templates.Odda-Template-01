8abf4b5705c4ece58dd835df4fc9d0dfe0590f89
xvichie <georgiandodo.dodoshka@gmail.com>
2025-01-01 22:23:41 +0400

    added tally watermark

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import lightTheme from "./themes";
 import Home from "./screens/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import { Outlet } from "react-router";
+import TallyTemplate from "./components/TallyTemplate";
 
 function App() {
   return (
-    <Flowbite theme={{theme: lightTheme}}>
+    <Flowbite theme={{ theme: lightTheme }}>
       <div className="font-mrgvlovani">
-          <Navbar />
-          <Outlet />
-          <Footer />
+        <TallyTemplate></TallyTemplate>
+        <Navbar />
+        <Outlet />
+        <Footer />
       </div>
     </Flowbite>
   );
